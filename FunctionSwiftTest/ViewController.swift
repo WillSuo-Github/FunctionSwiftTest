ee4108d80b76b9d010ee018c9b09257e5bcb509e
greatLock <a739002611@gmail.com>
2018-06-03 15:06:38 +0800

    Initial Commit

---
+++ FunctionSwiftTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunctionSwiftTest
+//
+//  Created by great Lock baidu on 2018/6/3.
+//  Copyright © 2018年 great Lock baidu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
