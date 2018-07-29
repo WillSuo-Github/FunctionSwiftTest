d0e5c1ee9369846ee7a7187061d40b4d99878ea4
greatLock <a739002611@gmail.com>
2018-07-29 18:48:38 +0800

    1

--- FunctionSwiftTest/EnumTest.swift
+++ FunctionSwiftTest/EnumTest.swift
@@ -29,7 +29,24 @@ indirect enum BinarySearchTree<Element: Comparable> {
     case node(BinarySearchTree<Element>, Element, BinarySearchTree<Element>)
 }
 
+extension Sequence {
+    func all(predicate: (Iterator.Element) -> Bool) -> Bool {
+        for x in self where !predicate(x) {
+            return false
+        }
+        return true
+    }
+}
+
 extension BinarySearchTree {
+    init() {
+        self = .leaf
+    }
+    
+    init(_ value: Element) {
+        self = .node(.leaf, value, .leaf)
+    }
+    
     var count: Int {
         switch self {
         case .leaf:
@@ -45,6 +62,57 @@ extension BinarySearchTree {
             return []
         case let .node(left, x, right):
             return [x] + left.elements + right.elements
+        }
+    }
+    
+    func reduce<A>(leaf leafF: A, note noteF: (A, Element, A) -> A) -> A {
+        switch self {
+        case .leaf:
+            return leafF
+        case let .node(left, x, right):
+            return noteF(left.reduce(leaf: leafF, note: noteF), x, right.reduce(leaf: leafF, note: noteF))
+        }
+    }
+    
+    var countR:Int {
+        return reduce(leaf: 0){1 + $0 + $2}
+    }
+    
+    var elementR: [Element] {
+        return reduce(leaf: []){$0 + [$1] + $2}
+    }
+    
+    var isBST: Bool {
+        switch self {
+        case .leaf:
+            return true
+        case let .node(left, x, right):
+            return left.elements.all{y in y < x}
+                && right.elements.all{y in y > x}
+                && left.isBST
+                && right.isBST
+        }
+    }
+    
+    func containts(_ x: Element) -> Bool {
+        switch self {
+        case .leaf:
+            return false
+        case let .node(left, y, right):
+            return left.containts(x)
+                || y == x
+                || right.containts(x)
+        }
+    }
+    
+    mutating func insert(_ x: Element) {
+        switch self {
+        case .leaf:
+            self = BinarySearchTree(x)
+        case .node(var left, let y, var right):
+            if x < y { left .insert(x) }
+            if x > y { right.insert(x) }
+            self = .node(left, x, right)
         }
     }
 }
